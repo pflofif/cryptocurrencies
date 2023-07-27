69b734bae332e24f70049d7515dcb6b4b9e04614
Bohdan Vivchar <bohdan@office.cquelle.de>
2023-07-27 22:16:04 +0300

    Register Pages in App, and finish Multi-Pages

---
+++ Cryptocurrencies/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,35 @@
+﻿using Cryptocurrencies.Core;
+using Cryptocurrencies.MVVM.View;
+
+namespace Cryptocurrencies.MVVM.ViewModel;
+
+public class MainViewModel : ObservableObject
+{
+    private HomeViewModel HomeViewModel { get; }
+    private InfoViewModel InfoViewModel { get; }
+    private object _currentView;
+
+    public RelayCommand HomeViewCommand { get; }
+    public RelayCommand InfoViewCommand { get; }
+
+    public MainViewModel()
+    {
+        InfoViewModel = new InfoViewModel();
+        HomeViewModel = new HomeViewModel();
+        
+        CurrentView = HomeViewModel;
+
+        HomeViewCommand = new RelayCommand(_ => CurrentView = HomeViewModel);
+        InfoViewCommand = new RelayCommand(_ => CurrentView = InfoViewModel);
+    }
+
+    public object CurrentView
+    {
+        get => _currentView;
+        private set
+        {
+            _currentView = value;
+            OnPropertyChanged();
+        }
+    }
+}
