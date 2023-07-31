033247a626e98238097f70365123cd77dd02a8a9
Bohdan Vivchar <bohdan@office.cquelle.de>
2023-08-01 00:18:19 +0300

    added to the InformationView.xaml the ability to show a information about cryptocurrency if it exists, or error message, if not

---
+++ Cryptocurrencies/Helpers/NullToVisibilityConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Cryptocurrencies.Helpers;
+
+public class NullToVisibilityConverter : IValueConverter
+{
+    public object Convert(object? value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return value is null
+            ? Visibility.Visible
+            : Visibility.Collapsed;
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return (value is Visibility visibility ? visibility : Visibility.Visible) == Visibility.Visible
+            ? value
+            : null;
+    }
+}
--- Cryptocurrencies/MVVM/ViewModel/InfoViewModel.cs
+++ Cryptocurrencies/MVVM/ViewModel/InfoViewModel.cs
@@ -1,6 +1,18 @@
-﻿namespace Cryptocurrencies.MVVM.ViewModel;
+﻿using Cryptocurrencies.Core;
+using Cryptocurrencies.MVVM.Model;
 
-public class InfoViewModel
+namespace Cryptocurrencies.MVVM.ViewModel;
+
+public class InfoViewModel : ObservableObject
 {
-    
+    private Cryptocurrency? _selectedCryptocurrency;
+    public Cryptocurrency? SelectedCryptocurrency
+    {
+        get => _selectedCryptocurrency;
+        set
+        {
+            _selectedCryptocurrency = value;
+            OnPropertyChanged();
+        }
+    }
 }
