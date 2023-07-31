2a8179a1f4a8c9f741942345c63b2e226a2caa6b
Bohdan Vivchar <bohdan@office.cquelle.de>
2023-08-01 00:15:38 +0300

    Add data binding to "Search" button

--- Cryptocurrencies/MVVM/ViewModel/MainViewModel.cs
+++ Cryptocurrencies/MVVM/ViewModel/MainViewModel.cs
@@ -1,28 +1,37 @@
-﻿using Cryptocurrencies.Core;
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Cryptocurrencies.Core;
+using Cryptocurrencies.MVVM.Model;
 using Cryptocurrencies.MVVM.View;
+using Cryptocurrencies.Services;
 
 namespace Cryptocurrencies.MVVM.ViewModel;
 
 public class MainViewModel : ObservableObject
 {
+    private readonly ICryptoApiService _apiService;
     private HomeViewModel HomeViewModel { get; }
     private InfoViewModel InfoViewModel { get; }
     private object _currentView = null!;
-
+    private string _searchQuery = "";
     public RelayCommand HomeViewCommand { get; }
     public RelayCommand InfoViewCommand { get; }
+    public ICommand SearchCommand { get; }
 
-    public MainViewModel(HomeViewModel homeViewModel, InfoViewModel infoViewModel)
+    public MainViewModel(HomeViewModel homeViewModel, InfoViewModel infoViewModel, ICryptoApiService apiService)
     {
         HomeViewModel = homeViewModel;
         InfoViewModel = infoViewModel;
-        
+        _apiService = apiService;
+
         CurrentView = HomeViewModel;
 
         HomeViewCommand = new RelayCommand(_ => CurrentView = HomeViewModel);
         InfoViewCommand = new RelayCommand(_ => CurrentView = InfoViewModel);
+        SearchCommand = new RelayCommand(async _ => await Search());
     }
-
     public object CurrentView
     {
         get => _currentView;
@@ -31,5 +40,22 @@ public class MainViewModel : ObservableObject
             _currentView = value;
             OnPropertyChanged();
         }
+    }
+
+    public string SearchQuery
+    {
+        get => _searchQuery; 
+        set
+        {
+            _searchQuery = value;
+            OnPropertyChanged();
+        }
+    }
+    
+    private async Task Search()
+    {
+        Cryptocurrency? crypto = await _apiService.GetCryptocurrencyAsync(SearchQuery);
+        InfoViewModel.SelectedCryptocurrency = crypto;
+        CurrentView = InfoViewModel;
     }
 }
